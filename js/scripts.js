121f422183971f9813e4ed1d636c420be775677a
securitysteven <24697316+securitysteven@users.noreply.github.com>
2026-02-22 14:51:22 +1300

    Refactor scroll trigger logic: improve variable declarations, enforce strict equality checks, and format animations for consistency.

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,24 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            location.pathname.replace(/^\//, "") ===
                 this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+            location.hostname === this.hostname
         ) {
-            var target = $(this.hash);
+            let target = $(this.hash);
             target = target.length
                 ? target
                 : $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
+                $("html, body").animate({
+                    scrollTop: target.offset().top,
+                }, 1000, "easeInOutExpo");
                 return false;
             }
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
+    // Closes a responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
